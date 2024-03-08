8e3c5332862d952de9c53901fd948a888a190fe9
ecmorgan360 <g20m8656@campus.ru.ac.za>
2024-03-08 16:17:06 +0200

    Added reverse complement functionality

--- myscript.py
+++ myscript.py
@@ -2,5 +2,10 @@
 
 print("I am from GitHub")
 
-for i in range(9):
-    print(i)
+def reverse_complement(sequence):
+    """Reverse complement a DNA sequence"""
+    revcomp = "" 
+    complement_bases = {"A":"T", "C":"G", "G":"C", "T":"A"}
+    for nt in sequence[::-1]:
+        revcomp += complement_bases[nt]
+    return revcomp
